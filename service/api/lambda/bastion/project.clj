d6638840bf68c53aa6226c05dbe0cec8d60b5a36
J.D. Hollis <jd@densityofspace.com>
2020-05-03 13:29:10 -0500

    Update all dependencies

--- service/api/lambda/bastion/project.clj
+++ service/api/lambda/bastion/project.clj
@@ -1,14 +1,14 @@
 (defproject bastion "0.1.0-SNAPSHOT"
   :description "bastion"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.async "0.4.490"]
-                 [com.cognitect.aws/api "0.8.171"]
-                 [com.cognitect.aws/endpoints "1.1.11.475"]
-                 [com.cognitect.aws/ec2 "684.2.380.0"]
-                 [com.cognitect.aws/ecs "683.2.374.0"]
-                 [com.amazonaws/aws-lambda-java-core "1.2.0"]
-                 [cheshire "5.8.1"]
-                 [digest "1.4.8"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "1.1.587"]
+                 [com.cognitect.aws/api "0.8.456"]
+                 [com.cognitect.aws/endpoints "1.1.11.774"]
+                 [com.cognitect.aws/ec2 "796.2.657.0"]
+                 [com.cognitect.aws/ecs "796.2.656.0"]
+                 [com.amazonaws/aws-lambda-java-core "1.2.1"]
+                 [cheshire "5.10.0"]
+                 [digest "1.4.9"]]
   :jvm-opts ["-Dclojure.compiler.elide-meta=[:doc]"
              "-Dclojure.compiler.direct-linking=true"]
   :profiles {:uberjar {:aot         :all
--- service/api/lambda/trigger-bastion-destruction/project.clj
+++ service/api/lambda/trigger-bastion-destruction/project.clj
@@ -1,8 +1,8 @@
 (defproject trigger-bastion-destruction "0.1.0-SNAPSHOT"
   :description "trigger-bastion-destruction"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.439"]]
-  :plugins [[lein-cljsbuild "1.1.7"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.753"]]
+  :plugins [[lein-cljsbuild "1.1.8"]]
   :cljsbuild {:builds [{:source-paths ["src"]
                         :compiler     {:output-to      "target/trigger-bastion-destruction/handler.js"
                                        :output-dir     "target/trigger-bastion-destruction"
